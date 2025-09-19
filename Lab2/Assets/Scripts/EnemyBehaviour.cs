bf7bbdc7118fe7fd8ce52de84c04a192ef8fa18a
Jimbo-10 <jimbo.leimapokpam@georgebrown.ca>
2025-09-19 11:29:57 -0400

    Enemy bouncing

--- Lab2/Assets/Scripts/EnemyBehaviour.cs
+++ Lab2/Assets/Scripts/EnemyBehaviour.cs
@@ -15,20 +15,29 @@ public class EnemyBehaviour : MonoBehaviour
     
     void Start()
     {
-        speed = Random.Range(speedRange.min, speedRange.max);
+        Reset();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        //transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        float xPos = Mathf.PingPong(Time.time * speed, horizontalScreenBoundary.max - horizontalScreenBoundary.min) + horizontalScreenBoundary.min;
+        transform.position = new Vector3(xPos, transform.position.y - speed * Time.deltaTime);
 
         if(transform.position.y < verticalScreenBoundary.min)
         {
-            transform.position = new Vector3(Random.Range(horizontalScreenBoundary.min, horizontalScreenBoundary.max),
+            Reset();
+           
+        }
+    }
+
+    private void Reset()
+    {
+        transform.position = new Vector3(Random.Range(horizontalScreenBoundary.min, horizontalScreenBoundary.max),
                                                           verticalScreenBoundary.max, transform.position.z);
 
-            speed = Random.Range(speedRange.min, speedRange.max);
-        }
+        speed = Random.Range(speedRange.min, speedRange.max);
     }
 }
